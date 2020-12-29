b399cc3364952216167e6484614c11ff84190682
nagaki <atsushi@nagaki.me>
2020-12-29 09:30:56 +0900

    スクロール制御を実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 import "../styles/globals.css";
 
+const noEvent = (e: Event | TouchEvent) => {
+  e.preventDefault();
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.addEventListener("touchmove", noEvent, { passive: false });
+    window.addEventListener("scroll", noEvent);
+  });
   return <Component {...pageProps} />;
 }
 
